3b45b9c117cadee39730cc3e312d579b8031a92a
Amina-Moufakkir <aminamouf22@gmail.com>
2023-03-06 12:53:45 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+import CartContainer from "./components/CartContainer"
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+export default App
